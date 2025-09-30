e56be3d772c47639b6058432f2d733c84b88d97f
st183962 <st183962@stud.uni-stuttgart.de>
2025-09-30 12:24:23 +0200

    importing sentences from data file

--- data.py
+++ data.py
@@ -1,19 +1,42 @@
 import wordnet
+import pandas as pd
+from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import word_tokenize
+
+
 class TargetWord:
-    def __init__(self,token):
+    def __init__(self, token, wn):
         self.token = token
-        self.wn = wordnet.WordNetInterface()
+        self.wn = wn
+
     def get_candidate_set(self):
-        candidates=set()
+        candidates = set()
         candidates.update(self.wn.get_synonyms(self.token))
         candidates.update(set(self.wn.get_hypernyms(self.token)))
         return candidates
 
+
 class Sentence:
-    def __init__(self,sentence,target):
+    def __init__(self, sentence, target, value,wn):
         self.tokens = word_tokenize(sentence)
-        self.target = TargetWord(target)
-        target_index = self.tokens.index(target)
-        self.context = self.tokens[:target_index]+self.tokens[target_index+1:]
-    
+        self.target = TargetWord(target,wn)
+        self.value = value
+        wnl = WordNetLemmatizer()
+        target_index=-1
+        if target in self.tokens:
+            target_index = self.tokens.index(target)
+        else:
+            for i, token in enumerate(self.tokens):
+                if target == wnl.lemmatize(token.lower(), pos="v"):
+                    target_index = i
+                    break
+        if(target_index<0):
+            raise ValueError("Target doesn't appear in sentence")
+        else:
+            self.context = self.tokens[:target_index] + self.tokens[target_index + 1 :]
+
+
+class DataSet:
+
+    def __init__(self, filepath, extraction_function):
+        self.sentences = extraction_function(filepath)
--- main.py
+++ main.py
@@ -1,4 +1,23 @@
-import target
+from data import DataSet,Sentence,TargetWord
+import pandas as pd
+from wordnet import WordNetInterface
+urban_dataset= "/projekte/semrel/Annotations/Figurative-Language/multilingual_EN_DE_SI_lit-fig_v-obj_abstract-concrete/English/example_sentences_verb-object.tsv"
+def urban_extractor(filepath):
+    sentences = []
+    fail_counter = 0
+    with open(filepath) as data:
+        for line in data:
+            datapoint = line.split("\t")
+            if datapoint[2]!="unsure" and datapoint[1]==datapoint[2]:
+                try:
+                    sentence = Sentence(datapoint[3],datapoint[0].split()[0],datapoint[2],wn)
+                    sentences.append(sentence)
+                except ValueError:
+                    print("Target word not in sentence")
+                    fail_counter+=1
+    print(len(sentences),fail_counter)
+    return sentences
+
 if __name__ == "__main__":
-    word = target.TargetWord("throw")
-    print(word.get_candidate_set())
+    wn = WordNetInterface()
+    DataSet(urban_dataset,urban_extractor)
