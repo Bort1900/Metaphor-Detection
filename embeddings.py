25b71cbc34d3af5f62ed70e926f536e36e3d3b90
st183962 <st183962@stud.uni-stuttgart.de>
2025-10-02 11:37:41 +0200

    new wrapper class for fasttext embeddings

--- embeddings.py
+++ embeddings.py
@@ -1,23 +1,26 @@
-# import fasttext
+import fasttext
 
 
-# class FasttextModel:
-#     def __init__(self, data_file, save_file, model_type):
-#         self.model = None
-#         self.save_file = save_file
-#         self.model_type = model_type
-#         self.data_file=data_file
-#         self.train()
+class FasttextModel:
+    def __init__(self, load_file):
+        self.model = fasttext.load_model(load_file)
+        self.load_file = load_file
 
-#     def train(self, min_count=1):
-#         model = fasttext.train_unsupervised(input=self.data_file, model=self.model_type, min_count=min_count)
-#         model.save_model(self.save_file)
-#         self.model = model
+    def get_input_vector(self, token):
+        return self.model.get_word_vector(token)
 
-#     @staticmethod
-#     def loadModel(load_file, model_type, alternative_filename=None):
-#         save_file = load_file if not alternative_filename else alternative_filename
-#         load = fasttext.load_model(load_file)
-#         model = FasttextModel(save_file, model_type)
-#         model.model = load
-#         return model
+    def get_output_vector(self, token):
+        return self.model.get_output_matrix[self.model.get_word_id(token)]
+
+    # def train(self, min_count=1):
+    #     model = fasttext.train_unsupervised(input=self.data_file, model=self.model_type, min_count=min_count)
+    #     model.save_model(self.save_file)
+    #     self.model = model
+
+    # @staticmethod
+    # def loadModel(load_file, model_type, alternative_filename=None):
+    #     save_file = load_file if not alternative_filename else alternative_filename
+    #     load =
+    #     model = FasttextModel(save_file, model_type)
+    #     model.model = load
+    #     return model
