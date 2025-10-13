b07c14af373d7de14b241c2c91921fff32caa54f
st183962 <st183962@stud.uni-stuttgart.de>
2025-10-13 19:18:14 +0200

    introducing target token attribute for accessing exact inflection in the sentence

--- data.py
+++ data.py
@@ -23,10 +23,12 @@ class Sentence:
         target_index = -1
         if target in self.tokens:
             target_index = self.tokens.index(target)
+            self.target_token = target
         else:
             for i, token in enumerate(self.tokens):
                 if target == wnl.lemmatize(token.lower(), pos="v"):
                     target_index = i
+                    self.target_token = token.lower()
                     break
         if target_index < 0:
             raise ValueError("Target doesn't appear in sentence")
