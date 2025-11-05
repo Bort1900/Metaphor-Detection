d39d1b6102e76b2a6b5da8cc4192c35e10f7504a
st183962 <st183962@stud.uni-stuttgart.de>
2025-11-05 17:51:15 +0100

    everything without pandas now more space less time i hope

--- swow_interface.py
+++ swow_interface.py
@@ -18,14 +18,20 @@ class SWOWInterface:
         self.response_file = "SWOW-EN.complete.20180827.csv"
         self.stops = stopwords.words("english")
         self.num_responses = number_of_responses
+        self.cues_to_responses, self.responses_to_cues = self.init_response_table()
         (
-            self.cues_to_responses,
-            self.responses_to_cues,
-            self.association_strength_table,
-        ) = self.init_tables()
-        self.index()
+            self.association_strength_matrix,
+            self.cue_indices,
+            self.response_indices,
+        ) = self.init_strength_table()
+        self.combined_cue_response_indices = {
+            token: indices[token]
+            for indices in [self.cue_indices, self.response_indices]
+            for token in indices
+        }
 
-    def init_tables(self):
+    def init_response_table(self):
+        space_regex = re.compile(f"\\s")
         cues_to_responses = dict()
         responses_to_cues = dict()
         with open(
@@ -34,55 +40,74 @@ class SWOWInterface:
             assocs.readline()
             for line in assocs:
                 values = [item.strip()[1:-1] for item in line.split(",")]
-                if len(values) < 18:
+                if len(values) != 18:
                     continue
                 if values[11] in cues_to_responses:
                     cues_to_responses[values[11]] += [
-                        re.sub(f"\\s", "_", values[15 + i])
+                        re.sub(space_regex, "_", values[15 + i])
                         for i in range(self.num_responses)
                     ]
                 else:
                     cues_to_responses[values[11]] = [
-                        re.sub(f"\\s", "_", values[15 + i])
+                        re.sub(space_regex, "_", values[15 + i])
                         for i in range(self.num_responses)
                     ]
                 for i in range(self.num_responses):
                     if values[15 + i] in responses_to_cues:
                         responses_to_cues[values[15 + i]].append(
-                            re.sub(f"\\s", "_", values[11])
+                            re.sub(space_regex, "_", values[11])
                         )
                     else:
                         responses_to_cues[values[15 + i]] = [
-                            re.sub(f"\\s", "_", values[11])
+                            re.sub(space_regex, "_", values[11])
                         ]
-        strengths = pd.read_csv(
-            os.path.join(self.work_dir, self.strength_file),
-            keep_default_na=False,
-            header=0,
-            names=["cue", "response", "num_response", "num_total", "strength"],
-            delimiter="\t",
-        )
-        strengths[["cue", "response"]] = strengths[["cue", "response"]].replace(
-            to_replace=re.compile(f"\\s"), value="_"
-        )
-        return cues_to_responses, responses_to_cues, strengths
 
-    def index(self):
-        cue_arr = self.association_strength_table["cue"].to_numpy()
-        response_arr = self.association_strength_table["response"].to_numpy()
-        cues, cue_indices, cue_counts = np.unique(
-            cue_arr, return_index=True, return_counts=True
-        )
-        self.cue_to_index = {
-            cue: (index, index + count)
-            for cue, index, count in zip(cues, cue_indices, cue_counts)
+        return (cues_to_responses, responses_to_cues)
+
+    def init_strength_table(self):
+        space_regex = re.compile(f"\\s")
+        pairs_to_strength = dict()
+        cue_to_index = dict()
+        responses = set()
+        with open(
+            os.path.join(self.work_dir, self.strength_file), "r", encoding="utf-8"
+        ) as strengths:
+            strengths.readline()
+            for line in strengths:
+                values = [value.strip() for value in line.split("\t")]
+                if len(values) != 5:
+                    continue
+                cue = re.sub(space_regex, "_", values[0])
+                response = re.sub(space_regex, "_", values[1])
+                strength = float(values[4])
+                if (cue, response) in pairs_to_strength:
+                    pairs_to_strength[(cue, response)] += strength
+                else:
+                    pairs_to_strength[(cue, response)] = strength
+                if cue not in cue_to_index:
+                    cue_to_index[cue] = len(cue_to_index)
+                responses.add(response)
+        responses.difference_update(cue_to_index.keys())
+        num_cues = len(cue_to_index)
+        response_to_index = {
+            response: i + num_cues for i, response in enumerate(responses)
         }
-        self.response_to_index = dict()
-        for i, response in enumerate(response_arr):
-            if response in self.response_to_index:
-                self.response_to_index[response].append(i)
+        num_responses = len(response_to_index)
+        association_strength_matrix = np.zeros(
+            [num_cues + num_responses, num_cues + num_responses]
+        )
+        for tuple in pairs_to_strength:
+            cue = tuple[0]
+            response = tuple[1]
+            cue_index = cue_to_index[cue]
+            if response in cue_to_index:
+                response_index = cue_to_index[response]
             else:
-                self.response_to_index[response] = [i]
+                response_index = response_to_index[response]
+            association_strength_matrix[cue_index, response_index] = pairs_to_strength[
+                tuple
+            ]
+        return association_strength_matrix, cue_to_index, response_to_index
 
     def get_candidate_set(self, cue):
         output = set()
@@ -92,86 +117,32 @@ class SWOWInterface:
             output.update(self.responses_to_cues[cue])
         return output.difference(self.stops)
 
-    def get_neighbour_nodes(self, token):
-        output = set()
-        output.update(
-            [
-                cue
-                for cue in self.association_strength_table[
-                    self.association_strength_table["response"] == token
-                ]["cue"]
-            ]
-        )
-        output.update(
-            [
-                response
-                for response in self.association_strength_table[
-                    self.association_strength_table["cue"] == token
-                ]["response"]
-            ]
-        )
-        return output
-
     def get_weighted_neighbours(self, token):
-        df = self.association_strength_table
-        cue_arr = df["cue"].to_numpy()
-        resp_arr = df["response"].to_numpy()
-        strength_arr = df["strength"].to_numpy()
-        cue_index = (0, 0)
-        response_index = []
-        if token in self.cue_to_index:
-            cue_index = self.cue_to_index[token]
-        if token in self.response_to_index:
-            response_index = self.response_to_index[token]
-        if not cue_index and not response_index:
-            return dict()
-        tokens = np.concatenate(
-            [
-                resp_arr[cue_index[0] : cue_index[1]],
-                cue_arr[response_index],
-            ]
-        )
-        strengths = np.concatenate(
-            [
-                strength_arr[cue_index[0] : cue_index[1]],
-                strength_arr[response_index],
-            ]
-        )
-        try:
-            unique, inverse = np.unique(tokens, return_inverse=True)
-        except TypeError as er:
-            print([token for token in tokens if type(token) != str])
-            raise TypeError(er.message)
-        sums = np.bincount(inverse, weights=strengths)
-        return dict(zip(unique, sums))
+        neighbours = self.get_candidate_set(token)
+        return {
+            neighbour: self.get_association_strength(token, neighbour)
+            for neighbour in neighbours
+        }
 
     def get_association_strength(self, cue, response):
-        value = list(
-            self.association_strength_table[
-                self.association_strength_table["cue"] == cue
-            ][self.association_strength_table["response"] == response]["strength"]
-        )
-        if len(value) == 0:
+        strength = 0
+        if (
+            cue not in self.combined_cue_response_indices
+            or response not in self.combined_cue_response_indices
+        ):
             return 0
-        return value[0]
+        cue_index = self.combined_cue_response_indices[cue]
+        response_index = self.combined_cue_response_indices[response]
+        strength += self.association_strength_matrix[cue_index, response_index]
+        strength += self.association_strength_matrix[response_index, cue_index]
+        return strength
 
     def get_association_strength_matrix(self, use_only_cues=True):
-        cues = self.association_strength_table["cue"].unique()
-        num_cues = len(cues)
-        cue_indices = {cue: i for i, cue in enumerate(cues)}
         if use_only_cues:
-            valid = self.association_strength_table[
-                self.association_strength_table["response"].isin(cues)
-            ]
+            num_cues = len(self.cue_indices)
+            return (
+                self.association_strength_matrix[:num_cues, :num_cues],
+                self.cue_indices,
+            )
         else:
-            responses = self.association_strength_table["response"].unique()
-            for response in responses:
-                if response not in cue_indices:
-                    cue_indices[response] = num_cues
-                    num_cues += 1
-            valid = self.association_strength_table
-        association_matrix = np.zeros([len(cue_indices), len(cue_indices)])
-        row_indices = valid["cue"].map(cue_indices)
-        col_indices = valid["response"].map(cue_indices)
-        association_matrix[row_indices, col_indices] = valid["strength"]
-        return association_matrix, cue_indices
+            return self.association_strength_matrix, self.combined_cue_response_indices
