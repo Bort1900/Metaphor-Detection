017fc1c2453b545c79dfbb068c02991831465a54
st183962 <st183962@stud.uni-stuttgart.de>
2025-11-08 11:43:17 +0100

    added comments and doc

--- data.py
+++ data.py
@@ -11,9 +11,10 @@ import numpy as np
 class Sentence:
     def __init__(self, sentence, target, value, phrase="unknown"):
         """
+        A sentence containing a phrase that's either metaphorical or literal
         sentence: string of the sentence
         target: the token or its lemma which is the target for the label
-        value: the label, 0: literal, 1: figurative
+        value: the label, 0: figurative, 1: literal, can also be 0: figurative, 1:unsure, 2:literal
         phrase: the phrase containing the target if known
         """
         self.sentence = sentence
@@ -27,6 +28,7 @@ class Sentence:
             self.target_index = self.tokens.index(target)
             self.target_token = target
         else:
+            # search for inflection of target
             for i, token in enumerate(self.tokens):
                 if target == wnl.lemmatize(token.lower(), pos="v"):
                     self.target_index = i
@@ -40,6 +42,11 @@ class Sentence:
             )
 
     def replace_target(self, new_target, split_multi_word=True):
+        """
+        create a new sentence where the target is replaced by another word
+        new_target: the word replacing the old target
+        split_multi_word: if flagged, this will look for multi word tokens(indicated by "_") and split them up to replace the old target with multiple words
+        """
         if split_multi_word:
             new_sentence = self.sentence.replace(
                 self.tokens[self.target_index], " ".join(new_target.split("_")), 1
@@ -66,13 +73,19 @@ class Sentence:
 
 
 class DataSet:
-
     def __init__(self, filepath, extraction_function, use_unsure):
+        """
+        A set of sentences
+        filepath: where the data that will be converted to Sentences is stored
+        extraction_function: function that specifically extracts Sentence instances from the provided datafile must have the use_unsure parameter
+        use_unsure: parameter for the extraction_function that specifies wether the Sentence values are binary or have a third "unsure" bin
+        """
         self.sentences = extraction_function(filepath, use_unsure)
 
     def get_splits(self, splits):
         """
-        returns train, dev and test splits of the data, proportions can be given in this order
+        returns train, dev and test splits of the data
+        splits: list of 3 values for train, dev and test split proportion in that order
         """
         if len(splits) != 3:
             raise ValueError(
@@ -94,4 +107,7 @@ class DataSet:
 class Vectors:
     @staticmethod
     def cos_sim(vec1, vec2):
+        """
+        simple cosine similarity function
+        """
         return float(np.dot(vec1, vec2) / (np.linalg.norm(vec1) * np.linalg.norm(vec2)))
