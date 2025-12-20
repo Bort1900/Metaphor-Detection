e1958309477e04002a7a2b8d59a57d26e544bf5c
st183962 <st183962@stud.uni-stuttgart.de>
2025-12-20 11:55:51 +0100

    node 2 vec embeddings

--- embeddings.py
+++ embeddings.py
@@ -2,13 +2,17 @@ import fasttext
 from sklearn.decomposition import PCA
 import numpy as np
 from wordnet_interface import WordNetInterface
+from swow_interface import SWOWInterface
 from nltk.corpus import wordnet as wn
 from nltk.corpus import stopwords
 from data import Sentence
 import time
 import os
+from tqdm import tqdm
 import torch
+import random
 from transformers import BertModel, BertTokenizer
+from gensim.models import Word2Vec, KeyedVectors
 
 
 class Embeddings:
@@ -115,7 +119,6 @@ class WordAssociationEmbeddings(Embeddings):
         """
         self.swow = swow
         self.load(index_file, embedding_file)
-        self.mean_vector = np.mean(self.embeddings, axis=0)
 
     @staticmethod
     def create_graph_embeddings(
@@ -180,7 +183,7 @@ class WordAssociationEmbeddings(Embeddings):
             if len(neighbouring_nodes) == 0 or total == 0:
                 raise KeyError(f"{token} is not in word association graph")
             map_factor = 1 / total
-            weighted_mean = np.zeros(self.mean_vector.shape)
+            weighted_mean = np.zeros_like(self.embeddings[0])
             for neighbour in neighbouring_nodes:
                 if neighbour in self.indices:
                     neighbour_index = self.indices[neighbour]
@@ -353,3 +356,249 @@ class BertEmbeddings(Embeddings):
         if len(embeddings) == 0:
             return self.get_sentence_vector(sentence)
         return torch.stack(embeddings).mean(dim=0)
+
+
+class Node2VecEmbeddings(Embeddings):
+    def __init__(self, loadfile: str, swow: SWOWInterface):
+        """
+        class for loading and returning node2vec embeddings
+
+        :param loadfile: The path to where the keyedvectors of the model are stored
+        :param swow: SWOWInterface used to retrieve neighbouring nodes
+        """
+        self.wordvectors = KeyedVectors.load(loadfile)
+        self.swow = swow
+
+    def get_input_vector(self, token, pos=None):
+        """
+        returns the standard embeddings
+        :param token: token for which embeddings are returned
+        :param pos: irrelevant
+        """
+        if token in self.wordvectors:
+            return self.wordvectors[token]
+
+        else:
+            neighbouring_nodes = self.swow.get_weighted_neighbours(token)
+            total = sum([weight for weight in neighbouring_nodes.values()])
+            if len(neighbouring_nodes) == 0 or total == 0:
+                raise KeyError(f"{token} is not in word association graph")
+            map_factor = 1 / total
+            weighted_mean = np.zeros(self.wordvectors.vector_size)
+            for neighbour in neighbouring_nodes:
+                if neighbour in self.wordvectors:
+                    weighted_mean += (
+                        neighbouring_nodes[neighbour]
+                        * self.wordvectors[neighbour]
+                        * map_factor
+                    )
+            return weighted_mean
+
+
+class Node2VecEmbeddingsCreator:
+    def __init__(self, graph: SWOWInterface, is_directed=False, p=1, q=1):
+        """
+        Class for creating Node2Vec Embeddings following the original implementation(slightly adapted) by Aditya Grover and Jure Leskovec.
+        See https://github.com/aditya-grover/node2vec/tree/master
+
+        :param graph: the graph as an instance of SWOWInterface
+        :param is_directed: whether the graph should be seen as directed (cues to responses)
+        :param p: return parameter, controls the likelihood of returning to the previous node during the random walk(smaller value-> more local walks)
+        :param q: in-out parameter, controls the likelihood of further graph exploration (smaller value-> more depth first behaviour, higher value-> more breadth first behaviour)
+        """
+
+        self.graph = graph
+        self.is_directed = is_directed
+        self.p = p
+        self.q = q
+        self.nodes = self.graph.get_nodes(only_cues=self.is_directed)
+        self.alias_nodes = dict()
+        self.alias_edges = dict()
+
+    def create_embeddings(
+        self,
+        save_file: str,
+        walks: list[list[str]],
+        dimensions: int,
+        window_size: int,
+        min_count=0,
+        sg=False,
+    ):
+        """
+        Creates and saves
+
+        :param save_file: where the word2vec model will be stored, word2vec format without file ending
+        :param walks: the random walks to train the word2vec model
+        :param dimensions: number of dimensions the embeddings will have
+        :param window_size: window around the node in the walk that will be taken for positive sampling
+        :param sg: whether the model should learn with Skip-gram or CBOW
+        """
+        model = Word2Vec(
+            sentences=walks,
+            vector_size=dimensions,
+            window=window_size,
+            sg=sg,
+        )
+        model.save(save_file + ".model")
+        wv = model.wv
+        wv.save(save_file + ".kv")
+
+    def node2vec_walk(self, walk_length: int, start_node: str) -> list[str]:
+        """
+        Simulate a random walk starting from start node.
+
+        :param walk_length: length of a random walk
+        :start_node:  the node to start from
+        """
+
+        walk = [start_node]
+
+        while len(walk) < walk_length:
+            cur = walk[-1]
+            cur_nbrs = sorted(
+                self.graph.get_neighbours(cur, directional=self.is_directed)
+            )
+            if len(cur_nbrs) > 0:
+                if len(walk) == 1:
+                    alias_node = self.get_alias_nodes(cur)
+                    walk.append(cur_nbrs[self.alias_draw(alias_node[0], alias_node[1])])
+                else:
+                    prev = walk[-2]
+                    alias_edge = self.get_alias_edge(prev, cur)
+                    next = cur_nbrs[
+                        self.alias_draw(
+                            alias_edge[0],
+                            alias_edge[1],
+                        )
+                    ]
+                    walk.append(next)
+            else:
+                break
+
+        return walk
+
+    def simulate_walks(self, num_walks: int, walk_length: int) -> list[list[str]]:
+        """
+        Repeatedly simulate random walks from each node.
+
+        :param num_walks: number of random walks performed from each node as starting point
+        :param walk_length: the length of the random walks
+        """
+        walks = []
+        nodes = [node for node in self.nodes]
+        print("Walk iteration:")
+        for walk_iter in range(num_walks):
+            print(str(walk_iter + 1), "/", str(num_walks))
+            random.shuffle(nodes)
+            for node in tqdm(nodes):
+                walks.append(
+                    self.node2vec_walk(walk_length=walk_length, start_node=node)
+                )
+        return walks
+
+    def get_alias_edge(self, src: str, dst: str) -> tuple[list[int], list[float]]:
+        """
+        Get the alias edge setup lists for a given edge, i.e. aliased probabilities for getting to the neighbours after going from src to dst
+        """
+        if (src, dst) in self.alias_edges:
+            return self.alias_edges[(src, dst)]
+        unnormalized_probs = []
+        total = 0
+        for dst_nbr in sorted(
+            self.graph.get_neighbours(dst, directional=self.is_directed)
+        ):
+            if dst_nbr == src:
+                strength = (
+                    self.graph.get_association_strength(
+                        dst, dst_nbr, directional=self.is_directed
+                    )
+                    / self.p
+                )
+                unnormalized_probs.append(strength)
+                total += strength
+            elif dst_nbr in self.graph.get_neighbours(
+                src, directional=self.is_directed
+            ):
+                strength = self.graph.get_association_strength(
+                    dst, dst_nbr, directional=self.is_directed
+                )
+                unnormalized_probs.append(strength)
+                total += strength
+            else:
+                strength = (
+                    self.graph.get_association_strength(
+                        dst, dst_nbr, directional=self.is_directed
+                    )
+                    / self.q
+                )
+                unnormalized_probs.append(strength)
+                total += strength
+        normalized_probs = [float(u_prob) / total for u_prob in unnormalized_probs]
+        self.alias_edges[(src, dst)] = self.alias_setup(normalized_probs)
+        return self.alias_edges[(src, dst)]
+
+    def get_alias_nodes(self, node: str):
+        """
+        get the aliased transition_probabilities for a node
+        """
+        if node in self.alias_nodes:
+            return self.alias_nodes[node]
+        unnormalized_probs = []
+        total = 0
+        for nbr in sorted(
+            self.graph.get_neighbours(node, directional=self.is_directed)
+        ):
+            strength = self.graph.get_association_strength(
+                node, nbr, directional=self.is_directed
+            )
+            unnormalized_probs.append(strength)
+            total += strength
+        normalized_probs = [float(u_prob) / total for u_prob in unnormalized_probs]
+        self.alias_nodes[node] = self.alias_setup(normalized_probs)
+        return self.alias_nodes[node]
+
+    @staticmethod
+    def alias_setup(probs):
+        """
+        Compute utility lists for non-uniform sampling from discrete distributions.
+        Refer to https://hips.seas.harvard.edu/blog/2013/03/03/the-alias-method-efficient-sampling-with-many-discrete-outcomes/
+        for details
+        """
+        K = len(probs)
+        q = np.zeros(K)
+        J = np.zeros(K, dtype=int)
+
+        smaller = []
+        larger = []
+        for kk, prob in enumerate(probs):
+            q[kk] = K * prob
+            if q[kk] < 1.0:
+                smaller.append(kk)
+            else:
+                larger.append(kk)
+
+        while len(smaller) > 0 and len(larger) > 0:
+            small = smaller.pop()
+            large = larger.pop()
+
+            J[small] = large
+            q[large] = q[large] + q[small] - 1.0
+            if q[large] < 1.0:
+                smaller.append(large)
+            else:
+                larger.append(large)
+
+        return J, q
+
+    @staticmethod
+    def alias_draw(J, q):
+        """
+        Draw sample from a non-uniform discrete distribution using alias sampling.
+        """
+        K = len(J)
+
+        kk = int(np.floor(np.random.rand() * K))
+        if np.random.rand() < q[kk]:
+            return kk
+        else:
+            return J[kk]
