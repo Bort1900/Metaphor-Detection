a27a374bbbe97765cd24dff1cb7e3209282fd09a
st183962 <st183962@stud.uni-stuttgart.de>
2025-12-30 15:11:04 +0100

    fixed ignore counter

--- main.py
+++ main.py
@@ -1,20 +1,35 @@
 from data import DataSet, Sentence
-from embeddings import FasttextModel, WordAssociationEmbeddings, BertEmbeddings
+from embeddings import (
+    FasttextModel,
+    WordAssociationEmbeddings,
+    BertEmbeddings,
+    Node2VecEmbeddings,
+    Node2VecEmbeddingsCreator,
+)
 import pandas as pd
-from model import MaoModel, ComparingModel, RandomBaseline, ContextualMaoModel
+from model import (
+    MaoModel,
+    ComparingModel,
+    RandomBaseline,
+    ContextualMaoModel,
+    NThresholdModel,
+)
 import re
 import numpy as np
 import time
 import os
 from wordnet_interface import WordNetInterface
 from swow_interface import SWOWInterface
+import torch
 
 
 def urban_extractor(filepath, use_unsure):
     sentences = []
     fail_counter = 0
     with open(filepath) as data:
+        i=0
         for line in data:
+            i+=1
             datapoint = line.split("\t")
             if (datapoint[2] != "unsure" and datapoint[1] == datapoint[2]) or (
                 datapoint[2] == "unsure" and use_unsure
@@ -48,7 +63,7 @@ def urban_extractor(filepath, use_unsure):
                     sentences += [verb_sentence, noun_sentence]
                 except ValueError:
                     fail_counter += 1
-    print(f"Ignored {fail_counter} of {len(sentences)}")
+    print(f"Ignored {fail_counter} of {i}")
     return sentences
 
 
@@ -57,7 +72,9 @@ def mohammad_extractor(filepath, use_unsure):
     fail_counter = 0
     with open(filepath) as data:
         data.readline()
+        i=0
         for line in data:
+            i+=1
             datapoint = line.split("\t")
             if len(datapoint) == 5 and (float(datapoint[4]) >= 0.7 or use_unsure):
                 if float(datapoint[4]) < 0.7:
@@ -78,111 +95,139 @@ def mohammad_extractor(filepath, use_unsure):
                     sentences.append(sentence)
                 except ValueError:
                     fail_counter += 1
-    print(f"Ignored {fail_counter} of {len(sentences)}")
+    print(f"Ignored {fail_counter} of {i}")
     return sentences
 
 
 if __name__ == "__main__":
+    RANDOM_SEED = 153
     DATA_DIR = "/projekte/semrel/WORK-AREA/Users/navid"
     WORDNET_DIR = "/resources/data/WordNet/WordNet-3.0_extract/"
     URBAN_DIR = "/projekte/semrel/Annotations/Figurative-Language/multilingual_EN_DE_SI_lit-fig_v-obj_abstract-concrete/English"
     SWOW_DIR = "/projekte/semrel/WORK-AREA/Users/navid/SWOW-EN18"
+    print(torch.cuda.get_device_name())
     print("loading interfaces")
-    # swow_r1 = SWOWInterface(
-    #     number_of_responses=1, strength_file="strength.SWOW-EN.R1.20180827.csv"
-    # )
-    # swow_r12 = SWOWInterface(
-    #     number_of_responses=2, strength_file="strengths_manually_r12.tsv"
-    # )
-    # swow_r123 = SWOWInterface(
-    #     number_of_responses=3, strength_file="strength.SWOW-EN.R123.20180827.csv"
-    # )
+    swow_r1 = SWOWInterface(
+        number_of_responses=1, strength_file="strength.SWOW-EN.R1.20180827.csv"
+    )
+    swow_r1_c2 = SWOWInterface(
+        number_of_responses=1,
+        strength_file="strength.SWOW-EN.R1.20180827.csv",
+        candidate_cap=2,
+    )
+    swow_r1_man = SWOWInterface(
+        number_of_responses=1,
+        strength_file="strengths_manually_r1.tsv",
+        candidate_cap=0,
+    )
     wn = WordNetInterface()
     print("loading embeddings")
     urban_dataset = "/projekte/semrel/Annotations/Figurative-Language/multilingual_EN_DE_SI_lit-fig_v-obj_abstract-concrete/English/example_sentences_verb-object.tsv"
     fasttext_dir = "/projekte/semrel/WORK-AREA/Users/navid/wiki.en.bin"
     fasttext_2_dir = "/projekte/semrel/WORK-AREA/Users/navid/cc.en.300.bin"
     mohammad_dataset = "/projekte/semrel/WORK-AREA/Users/navid/Metaphor-Emotion-Data-Files/Data-metaphoric-or-literal.txt"
-    # swow_embeddings_r1 = WordAssociationEmbeddings(
-    #     swow=swow_r1,
-    #     index_file="cue_indices_r1.tsv",
-    #     embedding_file="/projekte/semrel/WORK-AREA/Users/navid/graph_embeddings/graph_embeddings_300_r1.npy",
-    # )
-    # swow_embeddings_r123 = WordAssociationEmbeddings(
-    #     swow=swow_r123,
-    #     index_file="cue_indices.tsv",
-    #     embedding_file=os.path.join(
-    #         DATA_DIR, "graph_embeddings/graph_embeddings_300.npy"
-    #     ),
-    # )
-
-    # swow_embeddings_r12 = WordAssociationEmbeddings(
-    #     swow=swow_r12,
-    #     index_file="cue_indices_manually_r12.tsv",
-    #     embedding_file="/projekte/semrel/WORK-AREA/Users/navid/graph_embeddings/graph_embeddings_manually_300_r12.npy",
-    # )
-    # swow_embeddings_r123 = WordAssociationEmbeddings(
-    #     swow=swow_r123,
-    #     index_file="cue_indices_manually_r123.tsv",
-    #     embedding_file="/projekte/semrel/WORK-AREA/Users/navid/graph_embeddings/graph_embeddings_manually_300_r123.npy",
-    # )
-    # embeddings = FasttextModel(load_file=fasttext_dir)
-    contextual_embeddings_l8 = BertEmbeddings([8])
-    contextual_embeddings_l12 = BertEmbeddings([12])
-    contextual_embeddings_l9_12 = BertEmbeddings([9, 10, 11, 12])
+    swow_embeddings_r1 = WordAssociationEmbeddings(
+        swow=swow_r1,
+        index_file="cue_indices_r1.tsv",
+        embedding_file="/projekte/semrel/WORK-AREA/Users/navid/graph_embeddings/graph_embeddings_300_r1.npy",
+    )
+    ft_embeddings = FasttextModel(load_file=fasttext_dir)
+    cont_embeddings_max = BertEmbeddings(
+        layers=[9, 10, 11, 12], use_phrase_embedding="max"
+    )
+    cont_embeddings_mean = BertEmbeddings(
+        layers=[9, 10, 11, 12], use_phrase_embedding="mean"
+    )
+    cont_embeddings = BertEmbeddings(layers=[9, 10, 11, 12], use_phrase_embedding=None)
+    n2v_sg = Node2VecEmbeddings(
+        os.path.join(DATA_DIR, "graph_embeddings/n2v_r1_sg.kv"), swow=swow_r1_man
+    )
     print("loading datasets")
     urban_data = DataSet(
-        filepath=urban_dataset, extraction_function=urban_extractor, use_unsure=False
+        filepath=urban_dataset,
+        extraction_function=urban_extractor,
+        use_unsure=True,
+        seed=RANDOM_SEED,
     )
     mohammad_data = DataSet(
         filepath=mohammad_dataset,
         extraction_function=mohammad_extractor,
-        use_unsure=False,
+        use_unsure=True,
+        seed=RANDOM_SEED,
     )
     print("loading models")
-    wn_bert = ContextualMaoModel(
+    wn_baseline_best = ContextualMaoModel(
         data=urban_data,
         candidate_source=wn,
-        mean_multi_word=True,
-        fit_embeddings=contextual_embeddings_l8,
-        score_embeddings=contextual_embeddings_l8,
-        restrict_pos=True,
+        mean_multi_word=False,
+        fit_embeddings=cont_embeddings_max,
+        score_embeddings=cont_embeddings_max,
+        use_context_vec=True,
+        apply_candidate_weight=False,
+        restrict_pos=["v"],
+        num_classes=3,
     )
-    moment = time.time()
-    for i in range(10, 20):
-        wn_bert.predict(wn_bert.test_data[i])
-        print("took", time.time() - moment)
-        moment = time.time()
-    # print("loading models")
-    # swow_model_r1=MaoModel(dev_data=urban_dev_data,test_data=urban_test_data,candidate_source=swow_r1,mean_multi_word=False,embeddings=swow_embeddings_r1,use_output_vec=False)
-    # swow_model_r12=MaoModel(dev_data=urban_dev_data,test_data=urban_test_data,candidate_source=swow_r12,mean_multi_word=False,embeddings=swow_embeddings_r12,use_output_vec=False)
-    # swow_model_r123=MaoModel(dev_data=urban_dev_data,test_data=urban_test_data,candidate_source=swow_r123,mean_multi_word=False,embeddings=swow_embeddings_r123,use_output_vec=False)
-    # print("evaluating")
-    # swow_model_r1.evaluate_per_threshold(start=0,steps=11,increment=0.1,save_file="model_results/swow_model_r1_manual.tsv")
-    # swow_model_r12.evaluate_per_threshold(start=0,steps=11,increment=0.1,save_file="model_results/swow_model_r12_manual.tsv")
-    # swow_model_r123.evaluate_per_threshold(start=0,steps=11,increment=0.1,save_file="model_results/swow_model_r123_manual.tsv")
-    # random_model = RandomBaseline(
-    #     dev_data=urban_dev_data,
-    #     test_data=urban_test_data,
-    #     candidate_source=wn,
-    #     embeddings=embeddings,
-    # )
-    # random_model.evaluate_per_threshold(
-    #     steps=0.1, save_file="model_results/random_baseline_wn.tsv"
-    # )
-    # random_swow_model = RandomBaseline(
-    #     dev_data=urban_dev_data,
-    #     test_data=urban_test_data,
-    #     candidate_source=swow,
-    #     embeddings=swow_embeddings_r1,
-    # )
-    # random_swow_model.evaluate_per_threshold(
-    # )
-    # comp_model=ComparingModel(dev_data=mohammad_dev_data,test_data=mohammad_test_data,literal_embeddings=embeddings,associative_embeddings=swow_embeddings_r1,num_classes=2,use_output_vec=False)
-    # comp_model.evaluate_per_threshold(
-    #    start=0,steps=11,increment=0.1, save_file="model_results/comp_model_r1_300.tsv"
-    # )
-    # contextual_embeddings=BertEmbeddings(layer=8)
-    # contextual_model = ContextualMaoModel(dev_data=urban_dev_data,test_data=urban_test_data,candidate_source=swow,mean_multi_word=True,embeddings=contextual_embeddings,num_classes=2)
-    # sent=urban_test_data[40]
-    # print(sent.sentence,sent.target,swow.get_candidate_set(sent.target),contextual_model.best_fit(sent),contextual_model.get_compare_value(sent),contextual_model.predict(sent))
+    swow_candidates_best = ContextualMaoModel(
+        data=urban_data,
+        candidate_source=swow_r1_c2,
+        mean_multi_word=False,
+        fit_embeddings=cont_embeddings_mean,
+        score_embeddings=cont_embeddings_mean,
+        use_context_vec=False,
+        apply_candidate_weight=True,
+        restrict_pos=["v"],
+        num_classes=3,
+    )
+    swow_model_best = NThresholdModel(
+        data=urban_data,
+        candidate_source=swow_r1,
+        mean_multi_word=False,
+        fit_embeddings=swow_embeddings_r1,
+        score_embeddings=swow_embeddings_r1,
+        use_output_vec=False,
+        apply_candidate_weight=False,
+        restrict_pos=None,
+        num_classes=3,
+    )
+    wn_candidates_best = NThresholdModel(
+        data=urban_data,
+        candidate_source=wn,
+        mean_multi_word=False,
+        fit_embeddings=n2v_sg,
+        score_embeddings=n2v_sg,
+        use_output_vec=False,
+        apply_candidate_weight=False,
+        restrict_pos=["v"],
+        num_classes=3,
+    )
+    print("evaluating")
+    # models = [
+    #     wn_baseline_best,
+    #     swow_candidates_best,
+    #     swow_model_best,
+    #     wn_candidates_best,
+    # ]
+    # names = [
+    #     "wn_baseline_best",
+    #     "swow_candidates_best",
+    #     "swow_model_best",
+    #     "wn_candidates_best",
+    # ]
+    # for model, name in zip(models, names):
+    #     model.train_thresholds(
+    #         increment=0.01, epochs=5, by_pos=["v"], exclude_extremes=[-1, 1]
+    #     )
+    #     print(model.evaluate(by_pos=["v"]))
+        # model.nfold_cross_validate(
+        #     n=10,
+        #     save_file="model_results/" + name + ".txt",
+        #     by_pos=["v"],
+        #     exclude_extremes=[-1, 1],     
+        # )
+        # model.draw_distribution_per_class(
+        #     save_file="model_distributions/" + name + ".png",
+        #     by_pos=["v"],
+        #     labels=["metaphorical", "literal"],
+        #     title=name,
+        # )
+        # model.evaluate_per_threshold(start=0.1,increment=0.05,steps=11,save_file="model_evaluations/"+name+".txt",by_pos=["v"])
